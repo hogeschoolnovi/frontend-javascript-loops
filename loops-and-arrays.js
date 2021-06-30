1b13ada193f7fa2e1e11518d2d9bbd19e56c45e4
tliefting <thierry_liefting@live.nl>
2021-06-30 21:06:27 +0200

    Done and ready for feedback! :)

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -1,44 +1,74 @@
 // ==========================================
-// Opdracht 1
-// Schrijf een script dat iedere naam in bovenstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
-// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
+// Opdracht 1. Maak een for-loop die 3 keer "Hoera!" in de terminal logt
+// Verwachte uitkomsten:
+// Hoera!
+// Hoera!
+// Hoera!
 // ==========================================
 
-const names = ["Henk", "Piet", "Fred", "Joop"];
-
-// Verwachtte uitkomsten:
-// Vóór het script zie je de originele waardes:
-// console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
-// Na jouw script zie je de aangepaste waardes:
-// console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
+for (let i = 0 ; i < 3; i++ ) {
+    console.log("Hoera!");
+}
 
 
 // ==========================================
-// Opdracht 2
-// Schrijf een script dat ieder getal in bovenstaande array met 3 vermenigvuldigd.
-// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
+// Verwachte uitkomsten:
+// loop...
+// loop...
+// loop...
+// loop...
+// klaar!
 // ==========================================
-
-const numbers = [2, 4, 5, 29, 38];
-
-// Verwachtte uitkomsten:
-// Vóór het script zie je de originele waardes:
-// console.log(numbers) geeft: [2, 4, 5, 29, 38];
-// Na jouw script zie je de aangepaste waardes:
-// console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
-
+for (let i = 0 ; i < 4; i++ ) {
+    console.log("loop");
+}
+console.log("klaar!")
 
 // ==========================================
-// Opdracht 3
-// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array het volume uitrekent.
-// Het volume is lengte x breedte x hoogte
-// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
+// Verwachte uitkomsten:
+// 0 is even
+// 1 is oneven
+// 2 is even
+// 3 is oneven
+// .... etc.
+// 15 is oneven
+
+// Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
+for (let i = 0 ; i < 16; i++ ) {
+    if(i % 2 == 0) {
+        console.log(i + " is even");
+    }
+    else {
+        console.log(i+" is oneven");
+    }
+}
 
-const squares = [30, 2, 8, 24, 11];
-
-// Verwachtte uitkomsten:
-// Vóór het script zie je de originele waardes:
-// console.log(numbers) geeft: [30, 2, 8, 24, 11];
-// Na jouw script zie je de aangepaste waardes:
-// console.log(numbers) geeft: [27000, 8, 512, 13824, 1331];
+// ==========================================
+// [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
+// Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal te staan.
+// Verwachte uitkomsten:
+// 0
+// 1
+// 2
+// > 3
+// > 4
+// > 5
+// >> 6
+// >> 7
+// >> 8
+// >> 9
+// ==========================================
+for (let i = 0 ; i < 10; i++ ) {
+    if(i <= 2){
+        console.log(i);
+    }
+    if(i >= 3 && i<6){
+        console.log("> "+i);
+    }
+    if(i >= 6){
+        console.log(">> "+i);
+    }
+}
